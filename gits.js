2703ae0a7b7fb4b9347e13ec1faf345f73ae94dc
Виктория Хамова <vikahamova@gmail.com>
2021-10-25 19:33:25 +0300

    3

--- gits.js
+++ gits.js
@@ -1,7 +1,6 @@
 let response = fetch('https://api.github.com/users/6thSence');
 if (response.ok) {
-//  let json = response.json();
-  console.log('hello');
+  console.log('hello!');
 } else {
     console.log('error lol');
 }
