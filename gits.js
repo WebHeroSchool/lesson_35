10f721841cbf37d4c9cc3bc88930078d2f9d5a0b
Виктория Хамова <vikahamova@gmail.com>
2021-11-02 02:49:31 +0300

    4

--- gits.js
+++ gits.js
@@ -1,6 +1,32 @@
-let response = fetch('https://api.github.com/users/6thSence');
-if (response.ok) {
-  console.log('hello!');
-} else {
-    console.log('error lol');
+
+let login = 'viktoriahamova'; //Вводим любой никнейм и выводятся данные
+let href = document.location.href;
+
+const url = function () {
+    username = login
+  return 'https://api.github.com/users/' + username;
 }
+let link = url();
+
+let rez = async function() {
+ let response = await fetch(link);
+if (response.ok) {
+  json = await response.json();
+
+  let name = document.createElement("h1");
+  name.innerHTML = json.name;
+  document.body.append(name);
+
+  let img = new Image();
+  img.src = json.avatar_url;
+  document.body.append(img);
+
+  let bio = document.createElement("p");
+  bio.innerHTML = json.bio;
+  document.body.append(bio);
+} else {
+  let element = document.createElement("h1");
+  element.innerHTML = "<h1>User is not found </h1>";
+  document.body.append(element);
+}
+}();
